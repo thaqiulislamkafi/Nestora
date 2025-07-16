c4c27cacd8b590b3bebe729fe0bde8370bfd70e7
thaqiulislamkafi <thaqiulislamkafi@tuta.io>
2025-07-16 06:16:54 +0600

    Axios Interceptor is integrated

--- src/Components/Hooks/useAxios.jsx
+++ src/Components/Hooks/useAxios.jsx
@@ -1,4 +1,8 @@
+import React, { use } from 'react';
+import { useNavigate } from 'react-router';
 import axios from "axios";
+import { AuthContext } from '../Provider/AuthProvider';
+import Loading from '../SharedElement/Loading';
 
 
 export const axiosSecure = axios.create({
@@ -10,6 +14,33 @@ export const axiosSecure = axios.create({
 // })
 
 const useAxios = ()=>{
+
+    const { currentUser, loading } = use(AuthContext);
+    const navigate = useNavigate();
+
+    if (loading)
+        return <Loading />
+
+    axiosSecure.interceptors.request.use(config => {
+        config.headers.Authorization = `Bearer ${currentUser?.accessToken}`;
+        return config
+    }, error => {
+        return Promise.reject(error)
+    })
+
+    axiosSecure.interceptors.response.use(res => {
+        return res;
+    }, error => {
+        const status = error.status;
+        if (status === 403) {
+            navigate('/unauthorized');
+        }
+        else if (status === 401) {
+            navigate('/login')
+        }
+
+        return Promise.reject(error);
+    })
 
     return axiosSecure ;
 };
